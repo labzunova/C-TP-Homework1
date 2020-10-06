499c297c368b0b13ff504f0114f7c526c355e396
Labzunova <dasha.labzunova@mail.ru>
2020-10-06 20:45:34 +0300

    plus sort

--- waybillSplit.c
+++ waybillSplit.c
@@ -1,7 +1,32 @@
 #include "waybillSplit.h"
+#include "productStruct.h"
+
+void quickSort(productStruct *arr, int first, int last)
+{
+    if (first < last)
+    {
+        int left = first, right = last, middle = arr[(left + right) / 2].price;
+        do
+        {
+            while (arr[left].price < middle) left++;
+            while (arr[right].price > middle) right--;
+            if (left <= right)
+            {
+                int tmp = arr[left].price;
+                arr[left].price = arr[right].price;
+                arr[right].price = tmp;
+                left++;
+                right--;
+            }
+        } while (left <= right);
+        quickSort(arr, first, right);
+        quickSort(arr, left, last);
+    }
+}
 
 int split(productStruct * waybill, productStruct * firstNew, productStruct * secondNew, int count){
-    // TODO: waybill sort
+    quickSort(waybill, 0, count - 1);
+
     firstNew[0] = waybill[0];
     secondNew[0] = waybill[1];
     int firstCounter = 1, secondCounter = 1;
